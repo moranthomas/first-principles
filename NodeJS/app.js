88c5bd1f8409f6ceb433f1aec6776e8adae9b18d
moranthomas <tom_moran@hotmail.com>
2018-10-31 15:42:13 +0000

    moving nodeJs stuff out of this repo

--- NodeJS/app.js
+++
@@ -1,4 +0,0 @@
-const request = require('request');
-request('http://stackabuse.com', function (err, res, body) {
-    console.log(body);
-});
--- NodeJS/http_hello.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function (req, res) {
-    res.writeHead(200);
-    res.end('Hello Thomas - This is the most basic node program!');
-});
-server.listen(8080);
