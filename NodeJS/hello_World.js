5e876a2322f5e9e0668a74338047d0782573f3a3
moranthomas <thomas@optimizme.com>
2019-01-03 16:58:52 +0000

    cleanup

--- NodeJS/hello_World.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-http.createServer(function (request, response) {
-//request.on("endnction () {
-	response.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
-	response.end('HelloP!');
-//});
-}).listen(8080);
